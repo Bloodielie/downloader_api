1aa27a652b9d0808ea9c0b2cf9e378e874070f0c
Bloodielie <49410211+Bloodielie@users.noreply.github.com>
2020-08-14 22:03:47 +0300

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import * as process from "process";
 
 const config = {
-  userName: process.env.user_name || "riopro2812",
-  password: process.env.password || "kik0kik1235",
+  userName: process.env.user_name || "NONE",
+  password: process.env.password || "NONE",
   port: process.env.port || 3000,
   redis_url: process.env.redis_url || "redis://localhost:6379",
 };
